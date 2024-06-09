af73b06b6580a96071491815c5f8a799a6a5517b
Ricardo Castillo <ricardoud2008@gmail.com>
2024-06-08 23:36:12 -0500

    ultimo commit 8 junio

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ app.listen(3000, function(){
 )
 
 const conexion = mysql.createConnection({
-    host: "bvxkl8kzdxxyvlg9pqox-mysql.services.clever-cloud.com",
-    database: "bvxkl8kzdxxyvlg9pqox",
-    user: "ugegb1addkivnaeq",
-    password: "4JR2cAZypsGUws3mzNLi"
+    host: "localhost",
+    database:"ingenieria",
+    user: "root",
+    password: "postgres"
   });
   
   // Conectar a la base de datos  
@@ -43,14 +43,44 @@ app.get("/inicio",function(req,res){
 
   res.render("inicio.ejs");
 
+
+
 });
 
 
 app.get("/inicio/estudiantes",function(req,res){
 
-  res.render("modulo1.ejs");  
+    res.render("modulo1.ejs"); 
+  // Realizar la consulta
+      conexion.query('SELECT * FROM estudiantes LIMIT 10', (err, results, fields) => {
+          if (err) {
+            console.error('Error en la consulta: ' + err.stack);
+            return;
+          }
+  
+        console.log("consulta realizada con exito");
+        console.log(results)
+        console.log(typeof(results))
+        console.log(results.length)
+       // res.send(results)
+      });
+    });
+  
 
+
+
+app.get("/inicio/estudiantes/consolidado",function(req,res){
+      
+      conexion.query('SELECT * FROM estudiantes LIMIT 10', (err, results, fields) => {
+          if (err) {
+            console.error('Error en la consulta: ' + err.stack);
+            return;
+          }
+      res.send(results)
+      });
 });
+
+
 
 
 app.get("/inicio/asignaturas",function(req,res){
@@ -59,17 +89,7 @@ app.get("/inicio/asignaturas",function(req,res){
 
 });
 
-// Realizar la consulta
-conexion.query('SELECT * FROM registro_alumnos where genero="M" LIMIT 3', (err, results, fields) => {
-if (err) {
-        console.error('Error en la consulta: ' + err.stack);
-        return;
-      }
 
-      console.log("consulta realizada con exito");
-      console.log(results)
-      console.log(typeof(results))
-    });
   
 
 app.post("/validar",function(req,res){
@@ -78,4 +98,14 @@ app.post("/validar",function(req,res){
     console.log(dat);
 });
 
+
+
+app.get('/inicio/estudiantes/consolidado/', (req, res) => {
+  const id = req.params.id; // Capturar el parámetro id de la URL
+  console.log('ID recibido:', id);
+
+
+
+  console.log(dat);
+});
 
